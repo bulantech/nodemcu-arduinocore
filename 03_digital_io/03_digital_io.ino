55967c7ba8920304fd2cc87803caf25905e7ea9c
bulantech <bulantech@gmail.com>
2018-04-14 20:47:49 +0700

    led blink

--- 03_digital_io/03_digital_io.ino
+++ 03_digital_io/03_digital_io.ino
@@ -7,4 +7,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED, 0); //led on
+  delay(1000);
+  digitalWrite(LED, 1); //led off
+  delay(1000);
 }
