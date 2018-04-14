0aeb6f695551d4fa1d9f6f731e956e33ff1d21ff
bulantech <bulantech@gmail.com>
2018-04-14 21:25:05 +0700

    digital define value

--- 03_digital_io/03_digital_io.ino
+++ 03_digital_io/03_digital_io.ino
@@ -1,12 +1,16 @@
 
+#define LED D0
+#define SW D3
+#define LED_ON 0
+#define LED_OFF 1
 void setup() {
   // put your setup code here, to run once:
-  pinMode(D0, OUTPUT);
-  pinMode(D3, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  pinMode(SW, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(D3)) digitalWrite(D0, 0);  
-  else digitalWrite(D0, 1); 
+  if(digitalRead(SW)) digitalWrite(LED, LED_ON);  
+  else digitalWrite(LED, LED_OFF); 
 }
