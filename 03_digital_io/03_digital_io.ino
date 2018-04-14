c26fb9f9c91ecf7a3e653111f3be0dd27666803e
bulantech <bulantech@gmail.com>
2018-04-14 20:45:09 +0700

    led on

---
+++ 03_digital_io/03_digital_io.ino
@@ -0,0 +1,10 @@
+#define LED D0
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, 0); //led on
+}
