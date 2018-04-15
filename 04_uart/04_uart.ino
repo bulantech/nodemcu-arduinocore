dce622a3be948f4613efb332e4a4bc119200d922
bulantech <bulantech@gmail.com>
2018-04-15 14:15:42 +0700

    Serial.println format all

--- 04_uart/04_uart.ino
+++ 04_uart/04_uart.ino
@@ -6,7 +6,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  count++;
-  Serial.println(count, DEC);
+  count = 15;
+  Serial.println(count, DEC); // 15
+  Serial.println(count, HEX); // F
+  Serial.println(count, OCT); // 17
+  Serial.println(count, BIN); // 1111
+  Serial.println(1.234, 0); // 1
+  Serial.println(1.234, 2); // 1.23
+  Serial.println("-------------------");
   delay(2000);
 }
