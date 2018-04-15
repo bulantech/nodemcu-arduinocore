430e4b487c553878d98767fc30e80dc424af3d61
bulantech <bulantech@gmail.com>
2018-04-15 23:14:59 +0700

    pwm D0 led + buzzer

--- 06_pwm/06_pwm.ino
+++ 06_pwm/06_pwm.ino
@@ -1,16 +1,20 @@
 
-#define LED D0                         
+#define LED D0  
+#define BUZZER D8                        
 
 void setup()
 {
   Serial.begin(115200);              //  setup serial
   pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop()
 {
-  analogWrite(LED,500);  
+  analogWrite(LED,500);
+  analogWrite(BUZZER,500);   
   delay(1000);
-  analogWrite(LED,1000);  
+  analogWrite(LED,1000); 
+  analogWrite(BUZZER,1000);  
   delay(1000);
 }
