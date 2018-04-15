9124cdf1ba6397b5bcbf49d10b9db1c9a746e09d
bulantech <bulantech@gmail.com>
2018-04-15 14:35:04 +0700

    Serial.read char

--- 04_uart/04_uart.ino
+++ 04_uart/04_uart.ino
@@ -2,12 +2,15 @@ int count;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("start.."); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  count++;
-  Serial.println(count, DEC); 
-  Serial.write(count);
-  delay(2000);
+  char incomingByte;
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("get > ");
+    Serial.println(incomingByte);
+  }
 }
