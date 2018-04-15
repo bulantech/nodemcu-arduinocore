1b951a02dd59179b077af18a84fcaa97cd220e8c
bulantech <bulantech@gmail.com>
2018-04-15 23:29:08 +0700

    pwm D0 led + buzzer 5 times

--- 06_pwm/06_pwm.ino
+++ 06_pwm/06_pwm.ino
@@ -1,7 +1,7 @@
 
 #define LED D0  
 #define BUZZER D8                        
-
+int count;
 void setup()
 {
   Serial.begin(115200);              //  setup serial
@@ -12,9 +12,12 @@ void setup()
 void loop()
 {
   analogWrite(LED,500);
-  analogWrite(BUZZER,500);   
+  if(count < 5) analogWrite(BUZZER,500);
+  else analogWrite(BUZZER,0);     
   delay(1000);
   analogWrite(LED,1000); 
-  analogWrite(BUZZER,1000);  
+  if(count < 5) analogWrite(BUZZER,1000);
+  else analogWrite(BUZZER,0);   
   delay(1000);
+  count++;
 }
