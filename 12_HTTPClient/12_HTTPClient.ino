c28f9e5c2dae31d38cdcaa7a06be3fcea2c5a95e
bulantech <bulantech@gmail.com>
2018-04-23 22:58:04 +0700

    RestClient https

--- 12_HTTPClient/12_HTTPClient.ino
+++ 12_HTTPClient/12_HTTPClient.ino
@@ -1,80 +1,50 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+#include <RestClient.h>
 
-const char* ssid     = "true_home2G_350";
-const char* password = "88f02350";
 
-const char* host = "api.github.com";
-const int httpsPort = 443;
+const char* ssid     = "********";
+const char* password = "********";
 
-// Use web browser to view and copy
-// SHA1 fingerprint of the certificate
-const char* fingerprint = "35 85 74 EF 67 35 A7 CE 40 69 50 F3 C0 F6 80 CF 80 3B 2E 19";
+RestClient client = RestClient("api.github.com",443,1);
 
 void setup() {
   Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
   Serial.println();
-  Serial.print("connecting to ");
+  Serial.println();
+  Serial.print("Connecting to ");
   Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
 }
 
+String response;
+
 void loop() {
-  delay(1000);
-  
-  // Use WiFiClientSecure class to create TLS connection  
-  WiFiClientSecure client;
-  Serial.print("connecting to ");
-  Serial.println(host);
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-  if (client.verify(fingerprint, host)) {
-    Serial.println("certificate matches");
-  } else {
-    Serial.println("certificate doesn't match");
-  }
-
-  String url = "/repos/esp8266/Arduino/commits/master/status";
-  Serial.print("requesting URL: ");
-  Serial.println(url);
-
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-
-  Serial.println("request sent");
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("headers received");
-      break;
-    }
-  }
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\"")) {
-    Serial.println("esp8266/Arduino CI successfull!");
-  } else {
-    Serial.println("esp8266/Arduino CI has failed");
-  }
-  Serial.println("reply was:");
-  Serial.println("==========");
-  Serial.println(line);
-  Serial.println("==========");
-  Serial.println("closing connection");
-
+  response = "";
+  client.setHeader("User-Agent: BuildFailureDetectorESP8266");
+  int statusCode = client.get("/repos/esp8266/Arduino/commits/master/status", &response);
+  Serial.print("Status code from server: ");
+  Serial.println(statusCode);
+  Serial.print("Response body from server: ");
+  Serial.println(response);
   delay(20000);
+  
 }
