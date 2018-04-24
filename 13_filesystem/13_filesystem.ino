04da9ff239a057ac27cb934e21b14d1a6b4afde5
Air <air-lidon@hotmail.com>
2018-04-24 15:08:47 +0700

    factory reset

--- 13_filesystem/13_filesystem.ino
+++ 13_filesystem/13_filesystem.ino
@@ -2,11 +2,16 @@
 #include <FS.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "";
-const char* password = "";
+const char* ssid     = "******";
+const char* password = "******";
+
+#define SW D3
+
+File configFile;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(SW, INPUT);
   delay(10);
   Serial.println();
   Serial.println();
@@ -16,13 +21,22 @@ void setup() {
     return;
   }
 
-  loadConfig();  
+  loadConfig();
+  Serial.print("Press key for reset config");
+  for(int i=5; i>0; i--) {
+    delay(1000); 
+    Serial.print(".");  
+  }  
+  Serial.println(); 
+  if(!digitalRead(SW)) {
+    resetConfig();  
+  }   
 
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.println(String(ssid)+" ,"+ password);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -39,11 +53,77 @@ void setup() {
 }
 
 void loop() {
-  delay(5000);  
+  Serial.print("Press key for update config");
+  for(int i=5; i>0; i--) {
+    delay(1000); 
+    Serial.print(".");  
+  }  
+  Serial.println(); 
+  if(!digitalRead(SW)) {
+    updateConfig();  
+  } 
+}
+
+bool resetConfig() {
+  Serial.println();
+  Serial.println("Reset config..");
+  configFile = SPIFFS.open("/config_reset.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config_reset file");
+    return false;
+  }
+
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("config_reset file size is too large");
+    return false;
+  }
+
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
+
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
+  configFile.readBytes(buf.get(), size);
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success()) {
+    Serial.println("Failed to parse config_reset file");
+    return false;
+  }
+
+  ssid = json["ap_name"];
+  password = json["ap_pass"];
+  
+  Serial.println("config.json data:");
+  json.printTo(Serial);
+  configFile.close();
+
+  configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    return false;
+  }
+  String newData;
+  Serial.println();
+  json.printTo(newData);
+  Serial.println("Reset newData ==========");
+  Serial.println(newData);
+  json.printTo(configFile);
+//  configFile.print(newData); 
+  configFile.close();
+  Serial.println(); 
+  
+  return true;
 }
 
 bool loadConfig() {
-  File configFile = SPIFFS.open("/config.json", "r");
+  Serial.println();
+  Serial.println("Load config..");
+  configFile = SPIFFS.open("/config.json", "r");
   if (!configFile) {
     Serial.println("Failed to open config file");
     return false;
@@ -63,6 +143,47 @@ bool loadConfig() {
   // use configFile.readString instead.
   configFile.readBytes(buf.get(), size);
 
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success()) {
+    Serial.println("Failed to parse config file");
+    return false;
+  }
+  String ap_name = json["ap_name"];
+  String ap_pass = json["ap_pass"];
+  ssid = ap_name.c_str();
+  password = ap_pass.c_str();
+  
+  Serial.println("config.json data:");
+  json.printTo(Serial);
+  configFile.close();
+  Serial.println(); 
+
+  return true;
+}
+
+bool updateConfig() {
+  Serial.println();
+  Serial.println("Update config..");
+  configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    return false;
+  }
+
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("Config file size is too large");
+    return false;
+  }
+
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
+
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
   configFile.readBytes(buf.get(), size);
 
   StaticJsonBuffer<200> jsonBuffer;
@@ -96,6 +217,7 @@ bool loadConfig() {
   json.printTo(configFile);
 //  configFile.print(newData); 
   configFile.close();
+  Serial.println(); 
   
   return true;
 }
